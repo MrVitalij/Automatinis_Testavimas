e70249a64f99af5b1e5734ddb0fabd3a612f41f6
MrVitalij <vitalijjancura@gmail.com>
2023-04-25 21:45:43 +0300

    skaiciuotuvas

--- Skaiciuotuvas/Program.cs
+++ Skaiciuotuvas/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Skaiciuotuvas
+namespace Skaiciuokles
 {
-    internal class Program
+    public class Skaiciuokles
     {
-        static void Main(string[] args)
+        public static int SudetisInt(int sk1, int sk2)
         {
+            return sk1 + sk2;
+        }
+
+        public static double SudetisDouble2(double sk1, double sk2)
+        {
+            return (double)sk1 + sk2;
+        }
+
+        public static double SudetisDouble3(double sk1, double sk2, double sk3)
+        {
+            return (double)sk1 + sk2 + sk3;
+        }
+
+        public static decimal SudetisDecimal3(decimal sk1, decimal sk2, decimal sk3)
+        {
+            return sk1 + sk2 + sk3;
+        }
+
+        public static int Atimtis(int sk1, int sk2)
+        {
+            return sk1 - sk2;
+        }
+
+        public static int Daugyba(int sk1, int sk2)
+        {
+            return sk1 * sk2;
+        }
+
+        public static decimal DalybaDecimal(int sk1, int sk2)
+        {
+            return (decimal)sk1 / sk2;
+        }
+
+        public static double DalybaDouble(int sk1, int sk2)
+        {
+            return (double)sk1 / sk2;
         }
     }
 }
