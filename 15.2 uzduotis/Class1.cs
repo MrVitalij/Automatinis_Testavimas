83d3c7cc768a2c5e6c490e37eb513206f808a015
MrVitalij <vitalijjancura@gmail.com>
2023-05-05 20:28:33 +0300

    15.2 uzduotis

---
+++ 15.2 uzduotis/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._2_uzduotis
+{
+    public class Class1
+    {
+    }
+}
